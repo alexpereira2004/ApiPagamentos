7180011899f1a2d43e89a97a8881efdf60916a37
alexpereira2004 <alexpereira2004@gmail.com>
2022-01-25 17:17:35 -0300

    Conversão de data e hora

---
+++ README.md
@@ -0,0 +1,3 @@
+
+### Convenções
+- A entrada de dados de data e hora deverá seguir um padrão para todo o projeto que se baseia no formato "dd/MM/yyyy H:mm:ss". 
--- src/main/java/br/com/lunacom/tools/config/MapperConfig.java
+++ src/main/java/br/com/lunacom/tools/config/MapperConfig.java
@@ -2,9 +2,14 @@ package br.com.lunacom.tools.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.modelmapper.AbstractConverter;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class MapperConfig {
@@ -19,6 +24,27 @@ public class MapperConfig {
     @Bean
     public ModelMapper modelMapper() {
         final ModelMapper modelMapper = new ModelMapper();
+
+        Converter<String, LocalDateTime> toLocalDateTime = new AbstractConverter<String, LocalDateTime>() {
+            @Override
+            protected LocalDateTime convert(String source) {
+                DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy H:mm:ss");
+                LocalDateTime localDate = LocalDateTime.parse(source, format);
+                return localDate;
+            }
+        };
+        modelMapper.addConverter(toLocalDateTime);
+
+        Converter<LocalDateTime, String> toString = new AbstractConverter<LocalDateTime, String>() {
+            @Override
+            protected String convert(LocalDateTime source) {
+                DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy H:mm:ss");
+                String stringDateTime = source.format(format);
+                return stringDateTime;
+            }
+        };
+        modelMapper.addConverter(toLocalDateTime);
+        modelMapper.addConverter(toString);
         return modelMapper;
     }
 
