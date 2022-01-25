bf8fe3e0b33af2a7c27ed26a084bea0f5617fc6f
alexpereira2004 <alexpereira2004@gmail.com>
2022-01-25 00:35:03 -0300

    Validacao request pagamento

--- src/main/java/br/com/lunacom/tools/domain/request/FormaPagamentoRequest.java
+++ src/main/java/br/com/lunacom/tools/domain/request/FormaPagamentoRequest.java
@@ -3,8 +3,11 @@ package br.com.lunacom.tools.domain.request;
 import br.com.lunacom.tools.domain.enumeration.FormaPagamentoEnum;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class FormaPagamentoRequest {
-    FormaPagamentoEnum tipo;
-    int parcelas;
+    private FormaPagamentoEnum tipo;
+    @NotNull(message = "Informe a quantidade de parcelas")
+    private String parcelas;
 }
