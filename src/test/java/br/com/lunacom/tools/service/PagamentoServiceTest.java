fc1849bc4d9c082cece6e960bd9963eb7a8a86a0
alexpereira2004 <alexpereira2004@gmail.com>
2022-01-29 00:15:57 -0300

    PagamentoServiceTest: Deve criar um pagamento

--- src/test/java/br/com/lunacom/tools/service/PagamentoServiceTest.java
+++ src/test/java/br/com/lunacom/tools/service/PagamentoServiceTest.java
@@ -1,23 +1,30 @@
 package br.com.lunacom.tools.service;
 
+import br.com.lunacom.tools.domain.entity.PagamentoEntity;
 import br.com.lunacom.tools.repository.DescricaoRepository;
 import br.com.lunacom.tools.repository.PagamentoRepository;
+import builder.PagamentoEntityBuilder;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 public class PagamentoServiceTest {
 
     PagamentoService service;
     @MockBean
-    PagamentoRepository pagamentoRepository;
+    PagamentoRepository repository;
     @MockBean
     DescricaoRepository descricaoRepository;
 
     @BeforeEach
     public void setUp() {
-        this.service = new PagamentoService(pagamentoRepository, descricaoRepository);
+        this.service = new PagamentoService(repository, descricaoRepository);
 
 //        this.mapper = new JsonResourceObjectMapper();
     }
@@ -25,6 +32,11 @@ public class PagamentoServiceTest {
     @Test
     @DisplayName("Deve criar um pagamento")
     public void salvarTest() {
+        final PagamentoEntity pagamentoEntity = PagamentoEntityBuilder.umPagamento().agora();
+        Mockito.when(repository.save(pagamentoEntity))
+                .thenReturn(pagamentoEntity);
 
+        PagamentoEntity response = service.salvar(pagamentoEntity);
+        Assertions.assertThat(response.getId()).isEqualTo(1064654654565451L);
     }
 }
